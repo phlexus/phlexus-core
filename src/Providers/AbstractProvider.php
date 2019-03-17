f7605fa52356873dff6a01ec4f93e1e18a3cb581
Anton Vasiliev <aionbase@gmail.com>
2019-03-17 21:01:57 +0000

    First version of AbstractProvider

--- src/Providers/AbstractProvider.php
+++ src/Providers/AbstractProvider.php
@@ -2,9 +2,54 @@
 
 namespace Phlexus\Providers;
 
+use LogicException;
+use Phalcon\DiInterface;
 use Phalcon\Mvc\User\Component;
 
 abstract class AbstractProvider extends Component implements ProviderInterface
 {
+    /**
+     * Provider name
+     *
+     * @var string
+     */
     protected $providerName;
+
+    final public function __construct(DiInterface $di)
+    {
+        if (!$this->providerName) {
+            throw new LogicException(
+                sprintf('Provider name is required! "%s" cannot have an empty name.', get_class($this))
+            );
+        }
+
+        $this->setDI($di);
+        $this->configure();
+    }
+
+    /**
+     * Get Provider name
+     *
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->providerName;
+    }
+
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        // Implement in child class
+    }
+
+    /**
+     * @return void
+     */
+    public function configure()
+    {
+        // Implement in child class
+    }
 }
