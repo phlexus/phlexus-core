3625228f8861bfa988e2009c93c5cbf271939d94
Anton Vasiliev <aionbase@gmail.com>
2019-03-17 22:07:17 +0000

    Added ViewProvider provider

---
+++ src/Providers/ViewProvider.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Providers;
+
+use Phalcon\Mvc\View;
+
+class ViewProvider extends AbstractProvider
+{
+    /**
+     * Provider name
+     *
+     * @var string
+     */
+    protected $providerName = 'view';
+
+    /**
+     * Register application service.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->di->setShared($this->providerName, function() {
+            $view = new View();
+
+            return $view;
+        });
+    }
+}
