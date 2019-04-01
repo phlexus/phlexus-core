2c0de45910975c34b4d480e82c3420a18f7ec316
Anton Vasiliev <aionbase@gmail.com>
2019-04-01 11:53:21 +0100

    Added modes for Application

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,8 @@
 
 namespace Phlexus;
 
+use InvalidArgumentException;
+use Phalcon\Cli\Console;
 use Phalcon\Di;
 use Phalcon\DiInterface;
 use Phalcon\Mvc\Application as MvcApplication;
@@ -9,13 +11,41 @@ use Phlexus\Providers\ConfigProvider;
 use Phlexus\Providers\DispatcherProvider;
 use Phlexus\Providers\ModulesProvider;
 use Phlexus\Providers\ProviderInterface;
-use Phlexus\Providers\ResponseProvider;
 use Phlexus\Providers\RegistryProvider;
+use Phlexus\Providers\ResponseProvider;
 use Phlexus\Providers\RouterProvider;
 use Phlexus\Providers\ViewProvider;
 
+/**
+ * Plexus Application
+ *
+ * @package Phlexus
+ */
 class Application
 {
+    /**
+     * Default MVC Mode
+     *
+     * Default behaviour of WEB Application
+     */
+    const MODE_DEFAULT = 'default';
+
+    /**
+     * Console Mode
+     *
+     * Used for tasks and cron jobs
+     */
+    const MODE_CLI = 'cli';
+
+    /**
+     * MC Mode
+     *
+     * Model and Controller mode without View.
+     * Controllers always returns array which is
+     * transformed into JSON and send to client.
+     */
+    const MODE_API = 'api';
+
     /**
      * The Dependency Injector
      *
@@ -31,6 +61,15 @@ class Application
     protected $app;
 
     /**
+     * Application mode
+     *
+     * Possible values: default and cli
+     *
+     * @var string
+     */
+    protected $mode;
+
+    /**
      * Application Service Providers
      *
      * @var ProviderInterface[]
@@ -39,17 +78,18 @@ class Application
 
     /**
      * Application constructor
+     *
      * @param string $mode
      * @param array $configs
      * @param array $vendorModules
      */
     public function __construct(
-        string $mode = 'default',
+        string $mode = self::MODE_DEFAULT,
         array $configs = [],
         array $vendorModules = []
     ) {
         $this->di = new Di();
-        $this->app = new MvcApplication($this->di);
+        $this->app = $this->createApplication($mode);
         $this->di->setShared('bootstrap', $this);
 
         Di::setDefault($this->di);
@@ -61,13 +101,18 @@ class Application
 
         $viewConfigs = $configs['view'] ?? [];
 
+        // Init Generic Service Providers
         $this->initializeProvider(new RegistryProvider($this->di));
         $this->initializeProvider(new ConfigProvider($this->di), $configs);
         $this->initializeProvider(new ModulesProvider($this->di), $modules);
         $this->initializeProvider(new RouterProvider($this->di));
-        $this->initializeProvider(new ViewProvider($this->di), $viewConfigs);
         $this->initializeProvider(new DispatcherProvider($this->di));
         $this->initializeProvider(new ResponseProvider($this->di));
+
+        // Init Mode Service Providers
+        if ($mode == self::MODE_DEFAULT) {
+            $this->initializeProvider(new ViewProvider($this->di), $viewConfigs);
+        }
 
         $this->app->setDI($this->di);
     }
@@ -117,5 +162,27 @@ class Application
         $this->providers[$provider->getName()] = $provider;
 
         return $this;
+    }
+
+    /**
+     * Create Internal Application
+     *
+     * @param string $mode
+     * @return Console|MvcApplication
+     * @throws InvalidArgumentException
+     */
+    protected function createApplication(string $mode)
+    {
+        $this->mode = $mode;
+
+        switch ($mode) {
+            case self::MODE_DEFAULT:
+            case self::MODE_API:
+                return new MvcApplication($this->di);
+            case self::MODE_CLI:
+                return new Console($this->di);
+            default:
+                throw new InvalidArgumentException('Invalid application mode: ' . $mode);
+        }
     }
 }
