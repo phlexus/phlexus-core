9b844124c6c940cd8298497f136765e44ceaae9a
Anton Vasiliev <aionbase@gmail.com>
2019-04-07 20:34:43 +0100

    Created ModelsManagerProvider service provider

---
+++ src/Providers/ModelsManagerProvider.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Providers;
+
+use Phalcon\Mvc\Model\Manager;
+
+class ModelsManagerProvider extends AbstractProvider
+{
+    /**
+     * Provider name
+     *
+     * @var string
+     */
+    protected $providerName = 'modelsManager';
+
+    /**
+     * Register application provider
+     *
+     * @param array $parameters
+     * @return void
+     */
+    public function register(array $parameters = [])
+    {
+        $this->di->setShared($this->providerName, function () {
+            /** @var \Phalcon\DiInterface $this */
+            $modelsManager = new Manager();
+            //$modelsManager->setEventsManager($this->getShared('eventsManager'));
+            return $modelsManager;
+        });
+    }
+}
