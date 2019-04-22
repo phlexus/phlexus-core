c62211671ccdc5c16e9a4a77d1a9e832ffdf1945
Anton Vasiliev <aionbase@gmail.com>
2019-04-22 21:40:50 +0100

    Added strtolower() to module key name

--- src/Providers/ModulesProvider.php
+++ src/Providers/ModulesProvider.php
@@ -60,6 +60,7 @@ class ModulesProvider extends AbstractProvider
             $moduleName = $matches[1];
             $className = self::PHLEXUS_NAMESPACE_PATTERN . $moduleName . '\\Module';
 
+            $moduleName = strtolower($moduleName);
             $modules[$moduleName] = [
                 'className' => $className,
                 'path' => $path[0] . DIRECTORY_SEPARATOR . 'Module.php',
