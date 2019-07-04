f314cfa76392effcc5afe20f7ec07da331db468c
Anton Vasiliev <aionbase@gmail.com>
2019-07-04 21:46:06 +0100

    Created AbstractEvent and EventException classes

---
+++ src/Event/AbstractEvent.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Event;
+
+use Phalcon\Di\Injectable;
+
+class AbstractEvent extends Injectable
+{
+}
---
+++ src/Event/EventException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Event;
+
+use Exception;
+
+class EventException extends Exception
+{
+}
